b1aaf8b158991d186b9bbae92e813e83891aa6bc
reffobic <133569987+reffobic@users.noreply.github.com>
2023-05-18 22:45:42 +0300

    Initial Commit

---
+++ Ass5/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Ass5
+//
+//  Created by Mohamed El-Refai on 18/05/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
